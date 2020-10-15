7393ca3e2439fa64ad15dcc68d60d73239c24725
Marco <marco.chianese93@gmail.com>
2020-10-15 15:29:09 +0200

    js-snack completo (da ricontrollare meglio peró)

---
+++ js-jsnack1/script.js
@@ -0,0 +1,16 @@
+//Il software deve chiedere per 5 volte all'utente di inserire un numero. Il programma stampa la somma di tutti i numeri
+
+var array = [];
+var somma = 0;
+// Inizio ciclo for
+
+
+for (i=0 ; i < 5 ; i++) {
+    numero= parseInt(prompt("scrivi un numero"));
+    array.push(numero);
+    somma= somma + numero ;
+    console.log(somma);
+
+}
+console.log(array);
+console.log("La somma degli elementi é:" + somma);
